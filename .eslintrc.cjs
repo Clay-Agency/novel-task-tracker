7fe841d994736ae1ca95157a898702ea3ccf0e63
Sangeon Yong <syong@Sangeonui-MacBookAir.local>
2026-02-19 02:56:01 +0900

    feat: scaffold React app and baseline tooling (refs #5)

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-hooks/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off'
+  },
+  overrides: [
+    {
+      files: ['**/*.test.{js,jsx}', 'src/test/**/*.js'],
+      globals: {
+        describe: 'readonly',
+        it: 'readonly',
+        expect: 'readonly'
+      }
+    }
+  ],
+  ignorePatterns: ['dist', 'node_modules']
+};
